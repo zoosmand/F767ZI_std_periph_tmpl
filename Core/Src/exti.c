db1b25792af50ad1be7fd1c400bda5d9123a409a
Dmitry Slobodchikov <zoosmand@gmail.com>
2023-08-30 20:36:07 +0300

    Fix SYSCFG register GPIOC 13

--- Core/Src/exti.c
+++ Core/Src/exti.c
@@ -22,21 +22,24 @@ uint32_t _EXTIREG_ = 0;
 
 /* Private functions ---------------------------------------------------------*/
 void EXTI_Init(void) {
-  /*** GPIO Button -  PC15 ***/
-  /* Mode */
+  /*** GPIO Button -  PC13 ***/
+  // /* Mode */
   // MODIFY_REG(BTN0_Port->MODER, BTN0_Pin_Mask, (_MODE_IN << BTN0_Pin_Pos * 2));
-  /* Speed */
-  // MODIFY_REG(GPIOG->OSPEEDR, (LED0_Pin_Mask | LED1_Pin_Mask | LED2_Pin_Mask), ((_SPEED_L << LED0_Pin_Pos * 2) | (_SPEED_L << LED1_Pin_Pos * 2) | (_SPEED_L << LED2_Pin_Pos * 2)));
-  /* Output type */
-  // MODIFY_REG(GPIOG->OTYPER, ((_OTYPE_PP << LED0_Pin_Pos) | (_OTYPE_PP << LED1_Pin_Pos) | (_OTYPE_PP << LED2_Pin_Pos)), ((_OTYPE_PP << LED0_Pin_Pos) | (_OTYPE_PP << LED1_Pin_Pos) | (_OTYPE_PP << LED2_Pin_Pos)));
-  /* Push mode */
+  // /* Speed */
+  // MODIFY_REG(BTN0_Port->OSPEEDR, BTN0_Pin_Mask, (_SPEED_L << BTN0_Pin_Pos * 2));
+  // /* Output type */
+  // MODIFY_REG(BTN0_Port->OTYPER, (_OTYPE_OD << BTN0_Pin_Pos), (_OTYPE_OD << BTN0_Pin_Pos));
+  // /* Push mode */
   // MODIFY_REG(BTN0_Port->PUPDR, BTN0_Pin_Mask, (_PUPD_NO << BTN0_Pin_Pos * 2));
 
+
+  SET_BIT(SYSCFG->EXTICR[3], SYSCFG_EXTICR4_EXTI13_PC);
   PREG_SET(EXTI->IMR, EXTI_IMR_MR13_Pos);
-  PREG_SET(EXTI->FTSR, EXTI_FTSR_TR13_Pos);
+  PREG_SET(EXTI->RTSR, EXTI_FTSR_TR13_Pos);
 
   /* EXTI Line 10-15 interrupt Init */
   NVIC_SetPriority(EXTI15_10_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
   NVIC_EnableIRQ(EXTI15_10_IRQn);
+  PREG_SET(EXTI->PR, EXTI_PR_PR13_Pos);
 
 }
